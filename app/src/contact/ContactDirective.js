90077d8564dc6121fa1ab61f71ccec6611d5eaac
laurentperroteau <laurentperroteau@gmail.com>
2016-07-31 18:26:55 +0200

    Get user selected in contact directive

--- app/src/contact/ContactDirective.js
+++ app/src/contact/ContactDirective.js
@@ -18,53 +18,55 @@
 
         var directive = {
             restrict: 'E',
-            scope: {},
-            /*scope: {
-             parentInfo: '<'
-             },*/
+            scope: {
+                user: '<' // one way
+            },
+            bindToController: true, // enables the inherited properties to be bound to the Controller, not the $scope Object.
             templateUrl: 'src/contact/view/contact.html',
             controllerAs: 'contact',
-            controller: function ContactController() {
+            controller: ContactController
+        };
 
-                var self = this;
+        function ContactController() {
 
-                self.makeContact  = makeContact;
+            var self = this;
+
+            self.makeContact  = makeContact;
+
+            /**
+             * Show the Contact view in the bottom sheet
+             */
+            function makeContact(selectedUser) {
+
+                $mdBottomSheet.show({
+                    controllerAs  : "vm",
+                    templateUrl   : 'src/contact/view/contactSheet.html',
+                    controller    : [ '$mdBottomSheet', ContactSheetController],
+                    parent        : angular.element(document.getElementById('content'))
+                }).then(function(clickedItem) {
+                    $log.debug( clickedItem.name + ' clicked!');
+                });
 
                 /**
-                 * Show the Contact view in the bottom sheet
+                 * User ContactSheet controller
                  */
-                function makeContact(selectedUser) {
+                function ContactSheetController( $mdBottomSheet ) {
+                    this.user = selectedUser;
+                    this.items = [
+                        { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
+                        { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
+                        { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
+                        { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
+                    ];
+                    this.contactUser = function(action) {
+                        // The actually contact process has not been implemented...
+                        // so just hide the bottomSheet
 
-                    $mdBottomSheet.show({
-                        controllerAs  : "vm",
-                        templateUrl   : 'src/contact/view/contactSheet.html',
-                        controller    : [ '$mdBottomSheet', ContactSheetController],
-                        parent        : angular.element(document.getElementById('content'))
-                    }).then(function(clickedItem) {
-                        $log.debug( clickedItem.name + ' clicked!');
-                    });
-
-                    /**
-                     * User ContactSheet controller
-                     */
-                    function ContactSheetController( $mdBottomSheet ) {
-                        this.user = selectedUser;
-                        this.items = [
-                            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
-                            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
-                            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
-                            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
-                        ];
-                        this.contactUser = function(action) {
-                            // The actually contact process has not been implemented...
-                            // so just hide the bottomSheet
-
-                            $mdBottomSheet.hide(action);
-                        };
-                    }
+                        $mdBottomSheet.hide(action);
+                    };
                 }
             }
-        };
+        }
 
         return directive;
     }
