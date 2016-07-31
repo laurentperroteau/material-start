5c1b9dad405a635fe79202e59e08e2b67e3f1b99
laurentperroteau <laurentperroteau@gmail.com>
2016-07-31 17:45:38 +0200

    Move contact component in separate directive

---
+++ app/src/contact/ContactDirective.js
@@ -0,0 +1,71 @@
+(function(){
+
+    angular
+        .module('users')
+        .directive('userContact', [
+            'userService', '$mdSidenav', '$mdBottomSheet', '$timeout', '$log',
+            ContactDirective
+        ]);
+
+    /**
+     * Main Controller for the Angular Material Starter App
+     * @param $scope
+     * @param $mdSidenav
+     * @param avatarsService
+     * @constructor
+     */
+    function ContactDirective( userService, $mdSidenav, $mdBottomSheet, $timeout, $log ) {
+
+        var directive = {
+            restrict: 'E',
+            scope: {},
+            /*scope: {
+             parentInfo: '<'
+             },*/
+            templateUrl: 'src/contact/view/contact.html',
+            controllerAs: 'contact',
+            controller: function ContactController() {
+
+                var self = this;
+
+                self.makeContact  = makeContact;
+
+                /**
+                 * Show the Contact view in the bottom sheet
+                 */
+                function makeContact(selectedUser) {
+
+                    $mdBottomSheet.show({
+                        controllerAs  : "vm",
+                        templateUrl   : 'src/contact/view/contactSheet.html',
+                        controller    : [ '$mdBottomSheet', ContactSheetController],
+                        parent        : angular.element(document.getElementById('content'))
+                    }).then(function(clickedItem) {
+                        $log.debug( clickedItem.name + ' clicked!');
+                    });
+
+                    /**
+                     * User ContactSheet controller
+                     */
+                    function ContactSheetController( $mdBottomSheet ) {
+                        this.user = selectedUser;
+                        this.items = [
+                            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
+                            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
+                            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
+                            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
+                        ];
+                        this.contactUser = function(action) {
+                            // The actually contact process has not been implemented...
+                            // so just hide the bottomSheet
+
+                            $mdBottomSheet.hide(action);
+                        };
+                    }
+                }
+            }
+        };
+
+        return directive;
+    }
+})();
