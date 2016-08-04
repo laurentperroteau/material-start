2115864283572c4c7fc1d15f122050b0fa4cec4a
laurentperroteau <laurentperroteau@gmail.com>
2016-08-04 07:42:48 +0200

    Remove makeUser function (now in ContactDirective)

--- app/src/users/UserController.js
+++ app/src/users/UserController.js
@@ -49,41 +49,6 @@
     function selectUser ( user ) {
       self.selected = angular.isNumber(user) ? $scope.users[user] : user;
     }
-
-    /**
-     * Show the Contact view in the bottom sheet
-     */
-    function makeContact(selectedUser) {
-
-        $mdBottomSheet.show({
-          controllerAs  : "vm",
-          templateUrl   : './src/users/view/contactSheet.html',
-          controller    : [ '$mdBottomSheet', ContactSheetController],
-          parent        : angular.element(document.getElementById('content'))
-        }).then(function(clickedItem) {
-          $log.debug( clickedItem.name + ' clicked!');
-        });
-
-        /**
-         * User ContactSheet controller
-         */
-        function ContactSheetController( $mdBottomSheet ) {
-          this.user = selectedUser;
-          this.items = [
-            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
-            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
-            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
-            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
-          ];
-          this.contactUser = function(action) {
-            // The actually contact process has not been implemented...
-            // so just hide the bottomSheet
-
-            $mdBottomSheet.hide(action);
-          };
-        }
-    }
-
   }
 
 })();
